b58503deed044fa9bd3cbeb73564c0028b3b5de0
Cypresskids6 <134014517+Cypresskids6@users.noreply.github.com>
2023-05-23 16:53:42 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,21 @@
+export let snackList = [
+    "broccoli.png",
+    "carrots.png",
+    "apples.png",
+    "off-brand-cheetos.png",
+    "doritos.png",
+    "takis.png",
+    "ribs.png",
+    "cookies.png",
+    "donuts.png",
+    "cupcake.png",
+    "birthday-cake.png",
+];
+
+export let decorationList = [
+    "./streamers.png",
+    "./goodiebags.png",
+    "./discoball.png",
+    "./partyhats.png",
+    "./glowbeachball.png",
+];
